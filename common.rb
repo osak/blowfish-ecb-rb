195811671c307abaa6aec9dafb7d82003f00727d
Osamu Koga (osa_k) <github@osak.jp>
2023-10-05 19:25:50 +0900

    Fix indent

--- common.rb
+++ common.rb
@@ -12,26 +12,26 @@ module Blowfish::ECB::Common
     # Note that unlike the original implementation, this method returns a new tuple representing the result
     # rather than destructively updating the input argument. 
     def encrypt(l, r, key)
-        l ^= key.p[0]
-        r = bf_enc(r, l, key.s, key.p[1])
-        l = bf_enc(l, r, key.s, key.p[2])
-        r = bf_enc(r, l, key.s, key.p[3])
-        l = bf_enc(l, r, key.s, key.p[4])
-        r = bf_enc(r, l, key.s, key.p[5])
-        l = bf_enc(l, r, key.s, key.p[6])
-        r = bf_enc(r, l, key.s, key.p[7])
-        l = bf_enc(l, r, key.s, key.p[8])
-        r = bf_enc(r, l, key.s, key.p[9])
-        l = bf_enc(l, r, key.s, key.p[10])
-        r = bf_enc(r, l, key.s, key.p[11])
-        l = bf_enc(l, r, key.s, key.p[12])
-        r = bf_enc(r, l, key.s, key.p[13])
-        l = bf_enc(l, r, key.s, key.p[14])
-        r = bf_enc(r, l, key.s, key.p[15])
-        l = bf_enc(l, r, key.s, key.p[16])
-        r ^= key.p[BF_ROUNDS + 1]
+      l ^= key.p[0]
+      r = bf_enc(r, l, key.s, key.p[1])
+      l = bf_enc(l, r, key.s, key.p[2])
+      r = bf_enc(r, l, key.s, key.p[3])
+      l = bf_enc(l, r, key.s, key.p[4])
+      r = bf_enc(r, l, key.s, key.p[5])
+      l = bf_enc(l, r, key.s, key.p[6])
+      r = bf_enc(r, l, key.s, key.p[7])
+      l = bf_enc(l, r, key.s, key.p[8])
+      r = bf_enc(r, l, key.s, key.p[9])
+      l = bf_enc(l, r, key.s, key.p[10])
+      r = bf_enc(r, l, key.s, key.p[11])
+      l = bf_enc(l, r, key.s, key.p[12])
+      r = bf_enc(r, l, key.s, key.p[13])
+      l = bf_enc(l, r, key.s, key.p[14])
+      r = bf_enc(r, l, key.s, key.p[15])
+      l = bf_enc(l, r, key.s, key.p[16])
+      r ^= key.p[BF_ROUNDS + 1]
 
-        [r & 0xffffffff, l & 0xffffffff]
+      [r & 0xffffffff, l & 0xffffffff]
     end
 
     # call-seq:
@@ -77,11 +77,11 @@ module Blowfish::ECB::Common
     # Note that due to limitation in Ruby, unlike what the original BF_ENC is doing,
     # this method returns the calculated value rather than destructively reassigning l.
     def bf_enc(l, r, s, p)
-        tmp =  s[0x0000+((r>>24)&0xff)]
-        tmp += s[0x0100+((r>>16)&0xff)]
-        tmp ^= s[0x0200+((r>> 8)&0xff)]
-        tmp += s[0x0300+((r    )&0xff)]
-        l ^ p ^ (tmp & 0xffffffff)
+      tmp =  s[0x0000+((r>>24)&0xff)]
+      tmp += s[0x0100+((r>>16)&0xff)]
+      tmp ^= s[0x0200+((r>> 8)&0xff)]
+      tmp += s[0x0300+((r    )&0xff)]
+      l ^ p ^ (tmp & 0xffffffff)
     end
   end
 end
