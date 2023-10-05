ea69cfcec0f2d93c052a2e9f9ad83dccdfa9508d
Osamu Koga (osa_k) <github@osak.jp>
2023-10-05 20:18:42 +0900

    Add cipher identity test

---
+++ test/cipher_test.rb
@@ -0,0 +1,11 @@
+require 'test/unit'
+require 'blowfish_ecb'
+
+class CipherTest < Test::Unit::TestCase
+  def test_identity
+    cipher = Blowfish::ECB::Cipher.new("cipher_test")
+    ["hogefugahige", "1234567890", (1..255).to_a.pack("C*")].each do |data|
+      assert_equal(data, cipher.decrypt(cipher.encrypt(data.force_encoding(Encoding::ASCII_8BIT))), "Failed at #{data}")
+    end
+  end
+end
